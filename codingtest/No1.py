6c92c5fa055dbd8cad64d33cc2313d8992982d16
unknown <p990805@naver.com>
2024-08-21 17:48:01 +0900

    20240821

---
+++ codingtest/No1.py
@@ -0,0 +1,50 @@
+def ally(A,B):
+    allyofA=[]
+    allyofB=[]
+    if A==B:
+        return -1
+    for i in range(len(allylst)):
+        if A in allylst[i] and B in allylst[i]:
+            return -1
+        elif A in allylst[i]:
+            allyofA=allylst[i]
+        elif B in allylst[i]:
+            allyofB=allylst[i]
+    if allyofA==[]:
+        allyofA.append(A)
+    if allyofB==[]:
+        allyofB.append(B)
+    for i in range(len(allyofA)):
+        for j in range(len(hostilelst)):
+            if allyofA[i] in hostilelst[j]:
+                if hostilelst[j][0] in allyofB or hostilelst[j][1] in allyofB:
+                    return -2
+    allylst.append(allyofA+allyofB)
+    return 1
+def attack(A,B,G):
+    for i in range(len(allylst)):
+        if A in allylst[i] and B in allylst[i]:
+            return -1
+    for i in range(N):
+        for j in range(N):
+            if Monarch[i][j]==A:
+                Ai,Aj=i,j
+    for i in range(N):
+        for j in range(N):
+            if Monarch[i][j]==B:
+                Bi,Bj=i,j            
+    if 1<abs(Ai-Bi) or 1<abs(Aj-Bj):
+        return -2
+    
+def recruit(A,Num,Option):
+    pass
+def destroy():
+    pass
+T=int(input())
+for tc in range(1,1+T):
+    N=int(input())
+    soldier=[list(map(int,input().split())) for i in range(N)]
+    Monarch=[input().split() for i in range(N)]
+    allylst=[]
+    hostilelst=[]
+    mornachidx=
--- codingtest/test.py
+++ codingtest/test.py
@@ -1,3 +1,26 @@
-lst=[1,1,2,3,4,5]
-lst.remove(1)
-print(lst)
+T = int(input())
+for tc in range(1, T + 1):
+    N, M, K = map(int, input().split())
+    seconds = list(map(int, input().split()))
+    seconds.sort()
+
+    i = 0
+    t = 0
+    boong = 0
+    flag = True
+    while i < N:
+        print(t, boong, seconds[i])
+        t += M
+        if seconds[i] < t and boong == 0:
+            flag = False
+            break
+        else:
+            boong += K
+        boong -= 1
+        i += 1
+
+
+    if flag:
+        print(f'#{tc} Possible')
+    else:
+        print(f'#{tc} Impossible')
